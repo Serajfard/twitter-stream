07486031340d8340f08ebe2eacab019501430384
Amin Serajfard <aminserajfard@gmail.com>
2022-01-25 15:15:04 +0800

    add swagger API documnet to project

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Log Aggregation API')
+    .setDescription('The log aggregation API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('logs')
+    .setBasePath('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, forbidNonWhitelisted: true
   }));
-  await app.listen(3000);
+
+  await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
