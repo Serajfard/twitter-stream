86695bc4077305fc6e04429868f486ab7a843dcd
Amin Serajfard <aminserajfard@gmail.com>
2022-01-25 10:18:36 +0800

    adding log module and controller to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LogModule } from './log/log.module';
 
 @Module({
-  imports: [],
+  imports: [LogModule],
   controllers: [AppController],
   providers: [AppService],
 })
