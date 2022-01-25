13f194e8ca18472a0a70421880be6cc29aa47fc9
Amin Serajfard <aminserajfard@gmail.com>
2022-01-25 16:05:06 +0800

    add socket functionalities to project

---
+++ src/app.gateway.ts
@@ -0,0 +1,29 @@
+import { Logger } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway, OnGatewayInit, WsResponse, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger("AppGateway(socket)");
+
+  afterInit(server: Server) {
+    this.logger.log("App Gateway is Initialized successfully!");
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  sendMessageToAllClients(message: string): void {
+    this.wss.emit("tweets", message);
+  }
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { sqlite_connection } from './config';
 import { LogsModule } from './logs/logs.module';
+import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(sqlite_connection),
     LogsModule],
   controllers: [],
-  providers: [],
+  providers: [AppGateway],
 })
 export class AppModule { }
